75f927363e26bc2234eb516d18a2f09f6b6856f0
Carl Lindberg (Xenit) <carl.lindberg@xenit.se>
2023-10-27 14:39:28 +0200

    innitial commit

---
+++ nat_gw/Setup-NATGW.ps1
@@ -0,0 +1,21 @@
+## Authenticate to Azure & select your subscription
+Connect-AzAccount
+Select-AzSubscription -SubscriptionId "<your_sub_id>"
+
+## Set some variables
+$rg = "rg-prod"
+$vnet = "vnet-prod"
+$subnet = "sn-prod"
+$location = "westeurope"
+$pip = "pip-natgw-prod"
+$natgwname = "ng-prod"
+
+## Create a virtual network
+New-AzResourceGroup -Name $rg -Location $location
+New-AzVirtualNetwork -Name "vnet-prod" -ResourceGroupName $rg -Location $location -AddressPrefix "10.0.0.0/24"
+
+## Create NAT Gateway, PIP and associate with subnet
+New-AzPublicIpAddress -Name $pip -ResourceGroupName $rg -Location $location -Sku "Standard" -AllocationMethod "Static" -Zone 1,2,3 
+New-AzNatGateway -Name $natgwname -ResourceGroupName $rg -Location $location -IdleTimeoutInMinutes 4 -Sku "Standard" -PublicIpAddress (Get-AzPublicIpAddress -Name $pip -ResourceGroupName $rg)
+$subnet_config = Add-AzVirtualNetworkSubnetConfig -Name $subnet -VirtualNetwork (Get-AzVirtualNetwork -Name $vnet -ResourceGroupName $rg) -NatGatewayId (Get-AzNatGateway -Name $natgwname -ResourceGroupName $rg).Id -AddressPrefix "10.0.0.0/26"
+$subnet_config | Set-AzVirtualNetwork
